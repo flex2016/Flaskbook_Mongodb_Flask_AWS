36d9906ac5aa821e88d1a9373676e280d9efd798
flex2016 <reloded2004@hotmail.com>
2018-06-14 10:56:23 -0400

    added templates and updated forms and views

--- user/forms.py
+++ user/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import Form
 from wtforms import validators, StringField, PasswordField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import ValidationError
+import re
 
 from user.models import User
 
@@ -27,6 +28,8 @@ class RegisterForm(Form):
     def validate_username(form, field):
         if User.objects.filter(username=field.data).first():
             raise ValidationError("Username already exists")
+        if not re.match("^[a-zA-Z0-9_-]{4,25}$", field.data):
+            raise ValidationError("Invalid username")
 
     def validate_email(form, field):
         if User.objects.filter(email=field.data).first():
